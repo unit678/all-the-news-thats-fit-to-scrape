f1e81da646268bcf1bd4d4730219719fa9cabf15
Garrion Martin <unit678@gmail.com>
2019-01-04 15:11:41 -0500

    changes

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
 # all-the-news-thats-fit-to-scrape
 
-The New York Time Scraper
+The NPR News Scraper
 Overview
-The New York Time Scraper (NYT Scraper for short) is a scraper app which captures the title, summary and image of articles of The New York Times. In this app, users are able to save their preferred articles, add notes and edit notes to one or multiple articles. Besides, app also provides search feature, allowing users to search in titles according to different key words.
 
-In this repository, you can see source code of NYT Scraper.
+In this repository, you can see source code of NPR Scraper.
 
 Key Dependencies
-request: enables cheerio to get access to front-end code of https://www.nytimes.com/section/world
+request: enables cheerio to get access to front-end code of NPR News
 
-cheerio: scrapes front-end code from https://www.nytimes.com/section/world
+cheerio: scrapes front-end code from NPR News
 
 mongoose: be in charge of database of scrap
 
--- server.js
+++ server.js
@@ -1,163 +1,34 @@
-// Dependencies
+//////////////////////////////////////////////// /* Imports */ //////////////////////////////////////////////////////////
+let express = require('express'); // Express Server
+let bodyParser = require('body-parser'); // Post Body Request
+let exphbs = require('express-handlebars'); // Templating Engine
+// let logger = require("morgan"); // Logger
+var db = require("./models"); // Require all models
+// let cheerio = require('cheerio'; // Web Scrapper
+// let mongoose = require('mongoose'; // MongoDB ORM
+// let db from "./models"; // Require all models
 
-var express = require("express");
-var method = require("method-override");
-var body = require("body-parser");
-var exphbs = require("express-handlebars");
-var mongoose = require("mongoose");
-var logger = require("morgan");
-var cheerio = require("cheerio");
-var request = require("request");
+/////////////////////////////////////////////// /* Set Up Variables*/ //////////////////////////////////////////////////////////
 
-// Mongoose
+let PORT = process.env.PORT || 8080; // Set Default Port for Express and Heroku
+let app = express(); // Initialize Express
 
-var Note = require("./models/Note");
-var Article = require("./models/Article");
-var databaseUrl = 'mongodb://heroku_1l1jhkz0:8uncp9ls1ml4kqliq8f8adjlbh@ds215502.mlab.com:15502/heroku_1l1jhkz0';
+/////////////////////////////////////////////// /* Configure middleware */ //////////////////////////////////////////////////////////
 
-if (process.env.MONGODB_URI) {
-    mongoose.connect(process.env.MONGODB_URI);
-}
-else {
-    mongoose.connect(databaseUrl);
-};
+// app.use(logger("dev")); // Use morgan logger for logging requests
+app.use(bodyParser.urlencoded({ extended: false })); // Use body-parser for handling form submissions
+app.use(bodyParser.json());
+app.use(express.static("public")); // Serve static content for the app from the "public" directory in the application directory.
 
-mongoose.Promise = Promise;
-var db = mongoose.connection;
-
-db.on("error", function(error) {
-    console.log("Mongoose Error: ", error);
-});
-
-db.once("open", function() {
-    console.log("Mongoose connection successful.");
-});
-
-
-var app = express();
-var port = process.env.PORT || 3000;
-
-// app set-ups
-
-app.use(logger("dev"));
-app.use(express.static("public"));
-app.use(body.urlencoded({extended: false}));
-app.use(method("_method"));
-app.engine("handlebars", exphbs({defaultLayout: "main"}));
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.listen(port, function() {
-    console.log("Listening on port " + port);
-})
+/////////////////////////////////////////////// /* Configure Routes */ //////////////////////////////////////////////////////////
+require("./controllers/webScrapperController.js")(app);
 
-// Routes
+/////////////////////////////////////////////// /* Execution */ //////////////////////////////////////////////////////////
 
-app.get("/", function(req, res) {
-    Article.find({}, null, {sort: {created: -1}}, function(err, data) {
-        if(data.length === 0) {
-            res.render("placeholder", {message: "There's nothing scraped yet. Please click \"Scrape For Newest Articles\" for fresh and delicious news."});
-        }
-        else{
-            res.render("index", {articles: data});
-        }
-    });
-});
-
-app.get("/scrape", function(req, res) {
-    request("https://www.nytimes.com/section/world", function(error, response, html) {
-        var $ = cheerio.load(html);
-        var result = {};
-        $("div.story-body").each(function(i, element) {
-            var link = $(element).find("a").attr("href");
-            var title = $(element).find("h2.headline").text().trim();
-            var summary = $(element).find("p.summary").text().trim();
-            var img = $(element).parent().find("figure.media").find("img").attr("src");
-            result.link = link;
-            result.title = title;
-            if (summary) {
-                result.summary = summary;
-            };
-            if (img) {
-                result.img = img;
-            }
-            else {
-                result.img = $(element).find(".wide-thumb").find("img").attr("src");
-            };
-            var entry = new Article(result);
-            Article.find({title: result.title}, function(err, data) {
-                if (data.length === 0) {
-                    entry.save(function(err, data) {
-                        if (err) throw err;
-                    });
-                }
-            });
-        });
-        console.log("Scrape finished.");
-        res.redirect("/");
-    });
-});
-
-app.get("/saved", function(req, res) {
-    Article.find({issaved: true}, null, {sort: {created: -1}}, function(err, data) {
-        if(data.length === 0) {
-            res.render("placeholder", {message: "You have not saved any articles yet. Try to save some delicious news by simply clicking \"Save Article\"!"});
-        }
-        else {
-            res.render("saved", {saved: data});
-        }
-    });
-});
-
-app.get("/:id", function(req, res) {
-    Article.findById(req.params.id, function(err, data) {
-        res.json(data);
-    })
-})
-
-app.post("/search", function(req, res) {
-    console.log(req.body.search);
-    Article.find({$text: {$search: req.body.search, $caseSensitive: false}}, null, {sort: {created: -1}}, function(err, data) {
-        console.log(data);
-        if (data.length === 0) {
-            res.render("placeholder", {message: "Nothing has been found. Please try other keywords."});
-        }
-        else {
-            res.render("search", {search: data})
-        }
-    })
-});
-
-app.post("/save/:id", function(req, res) {
-    Article.findById(req.params.id, function(err, data) {
-        if (data.issaved) {
-            Article.findByIdAndUpdate(req.params.id, {$set: {issaved: false, status: "Save Article"}}, {new: true}, function(err, data) {
-                res.redirect("/");
-            });
-        }
-        else {
-            Article.findByIdAndUpdate(req.params.id, {$set: {issaved: true, status: "Saved"}}, {new: true}, function(err, data) {
-                res.redirect("/saved");
-            });
-        }
-    });
-});
-
-app.post("/note/:id", function(req, res) {
-    var note = new Note(req.body);
-    note.save(function(err, doc) {
-        if (err) throw err;
-        Article.findByIdAndUpdate(req.params.id, {$set: {"note": doc._id}}, {new: true}, function(err, newdoc) {
-            if (err) throw err;
-            else {
-                res.send(newdoc);
-            }
-        });
-    });
-});
-
-app.get("/note/:id", function(req, res) {
-    var id = req.params.id;
-    Article.findById(id).populate("note").exec(function(err, data) {
-        res.send(data.note);
-    });
+app.listen(PORT, ()=>{
+    console.log(`App listening on PORT ${PORT}`);
 });
